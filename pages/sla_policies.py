f375bfec45062f2b74019302ed71f1e7bd104360
Avinash Kunnath <avinash.kunnath@fivetran.com>
2023-10-27 13:54:20 -0700

    fix SLA achievement

--- pages/sla_policies.py
+++ pages/sla_policies.py
@@ -33,8 +33,9 @@ else:
             col1, col2, col3 = st.columns(3)
             with col1:
                 st.subheader('SLA achievement rate')
-                achieved_sla_count = len(data_date_filtered[data_date_filtered['is_sla_breach'] == 0])
-                sla_achievement_rate = (data_date_filtered['achieved_sla_count'] / len(data_date_filtered)) * 100
+                sla_achieved_count = len(data_date_filtered.query("`is_sla_breach` == 0"))
+                total_ticket_count = data_date_filtered['ticket_id'].nunique()  # Calculate the total number of distinct ticket IDs
+                sla_achievement_rate = (sla_achieved_count / total_ticket_count) * 100
                 st.metric("SLA achievement rate",  value=f'{sla_achievement_rate:.2f}%', delta=None, delta_color="normal", help=None, label_visibility="visible")
 
             with col2:
